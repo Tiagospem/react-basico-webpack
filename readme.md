81b7d8c304039efcd014abc6e1bd09713c04e438
Tiagospem <tiagospem@gmail.com>
2019-08-27 09:50:53 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+#PRIMEIRO PROJETO EM REACT JS
+- conceitos básicos sobre a estrutura react + webpack + loaders
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+import rafa from './assets/img/rafa.jpg'
+
+function App(){
+  return (
+    <div>
+      <h1>Primeiro App</h1>
+      <img src={rafa} width="200" alt=""/>
+    </div>
+  )
+}
+
+export default App;
